58784b7e6ef23f99d3c9db307f6931592d8f6108
root <root@ip-172-31-40-200.ec2.internal>
2024-11-28 15:45:17 +0000

    commiting files

--- data1.sh
+++ data1.sh
@@ -5,5 +5,5 @@ yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
 yum install git -y
-sudo git clone https://github.com/Spandana115/Anticafe.git
-sudo mv Anticafe/2126_antique_cafe/* /var/www/html/
+sudo git clone https://github.com/Spandana115/kider.git
+sudo mv kider/preschool-website-template/* /var/www/html/
